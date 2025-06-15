6f5c128bfa8a761bf68e2a4a2e72dea36fe5874a
AlanL97 <alanl97@nycstudents.net>
2025-06-15 14:13:26 +0000

    final project

--- JavaAPIProject/src/main/java/com/example/App.java
+++ JavaAPIProject/src/main/java/com/example/App.java
@@ -13,51 +13,68 @@ import java.util.Scanner;
  */
 public class App 
 {
-    public static void main( String[] args ) throws Exception
+    //Main entry point for the program
+    public static void main(String[] args) throws Exception
     {
+        //declares and initializes a CollegeList object
         CollegeList list = new CollegeList();
         Scanner scan = new Scanner(System.in);
         boolean bool = true;
         boolean bool2 = true;
         list.loadData();
         System.out.println("Welcome to the College List App\nYou can search for colleges, view information on colleges, and add them to a college list");
+        //while loop so the program continuously runs unless the user enters 3 to exit
         while (bool) {
+            //prints a line asking for the user to input an integer
             System.out.println("--------------------------------");
             System.out.println("1. Search\n2. View List\n3. Exit");
             System.out.print("Enter an integer: ");
+            //takes in user input in the form of an integer
             int choice = scan.nextInt();
             scan.nextLine();
+            //if the user inputs 1, calls search()
             if (choice == 1) {
                 search(list);
             } else if (choice == 2) {
                 bool2 = true;
+                //while loop so the user is always on the college list menu unless the user enters 4 to exit
                 while (bool2) {
+                    //if the list is empty, prints a line telling the user that their list is empty and breaks, returning the user to the main menu
                     if (list.getList().size() == 0) {
                         System.out.println("Your list is empty");
                         break;
                     }
+                    //calls viewList() on list
                     list.viewList();
                     System.out.println("--------------------------------");
                     System.out.println("1. View School Info\n2. Remove School\n3. Clear List\n4. Exit");
                     System.out.print("Enter an integer: ");
+                    //takes in user input in the form of an integer
                     int viewChoice = scan.nextInt();
                     scan.nextLine();
+                    //if the user inputs 1, asks the user to choose a school in their list to view
                     if (viewChoice == 1) {
                         System.out.print("Choose a school you would like to view: ");
                         int idx = scan.nextInt();
                         scan.nextLine();
+                        //calls viewInfo on list with the user's input as the index to view a school's info
                         list.viewInfo(idx);
+                    //if the user inputs 2, asks the user to choose a school in their list to remove
                     } else if (viewChoice == 2) {
                         System.out.print("Choose a school you would like to remove: ");
                         int idx2 = scan.nextInt();
                         scan.nextLine();
+                        //calls removeCollege() on list with the user's input as the index to remove a college
                         list.removeCollege(idx2);
+                    //if the user inputs 3, calls clear() on list
                     } else if (viewChoice == 3) {
                         list.clear();
+                    //if the user inputs 4, bool2 is set to false, stopping the while loop and returning the user to the main menu
                     } else {
                         bool2 = false;
                     }
                 }
+            //if the user inputs 3, bool is set to false and saveData() is called on list. The while loop stops and the program ends
             } else {
                 bool = false;
                 list.saveData();
@@ -88,32 +105,47 @@ public class App
         return content.toString(); //return the content as a string
     }
 
+    //prints a list of school names from a search and allows the user to view information on a specific college from the search result
     public static void getSchoolInfo(String schoolName, CollegeList list) throws Exception {
         Scanner scan = new Scanner(System.in);
         boolean bool = true;
+        //declares and initializes a String with the string returned from calling getAPI()
         String jsonString = getAPI(schoolName);
+        //declares and initializes a JSONObject with the String jsonString
         JSONObject obj = new JSONObject(jsonString);
+        //declares and initializes a JSONArray of the JSONObject obj with the key "results"
         JSONArray arr = obj.getJSONArray("results");
+        //declares and initializes a new JSONObject
         JSONObject resultsObj = new JSONObject();
         System.out.println("--------------------------------");
+        //while loop so the user can view multiple schools from one search result
         while (bool) {
+            //iterates through every element in arr
             for (int i = 0; i < arr.length(); i++) {
+                //sets resultsObj to the JSONObject in arr at index i
                 resultsObj = arr.getJSONObject(i);
+                //sets a String to the String in resultsObj with the key "school.name"
                 String name = resultsObj.getString("school.name");
+                //prints the name of the school at index i with the index
                 System.out.println(name + ", Index: " + i);
             } 
+            //if arr is empty, prints a line telling the user that their school could not be found and break, ending the for loop and the method
             if (arr.length() == 0) {
                 System.out.println("Sorry, we could not find the school you're looking for");
                 break;
             }
             System.out.print("Please choose a school to view (index): ");
+            //takes in user input in the form of an integer
             int idx = scan.nextInt();
             scan.nextLine();
             System.out.println("--------------------------------");
+            //sets resultsObj to the JSONObject in arr at index idx, the user's input
             resultsObj = arr.getJSONObject(idx);
+            //prints school information such as school name, city, state, admission rate, etc. by calling resultsObj.getString() with the corresponding key
             System.out.println("School Name: " + resultsObj.getString("school.name"));
             System.out.println("City: " + resultsObj.getString("school.city"));
             System.out.println("State: " + resultsObj.getString("school.state"));
+            //uses try catch because sometimes a college's info is null, so a try catch prevents an Exception
             try {
                 System.out.println("Admission Rate: " + resultsObj.getFloat("latest.admissions.admission_rate.overall"));
             } catch (Exception e) {
@@ -139,27 +171,34 @@ public class App
             } catch (Exception e) {
                 System.out.println("Student Size: null");
             }
+            //prints a line asking if the user would like to add this college to their list
             System.out.print("Would you like to add this college to your college list? (yes/no): ");
+            //takes in user input in the form of a String
             String add = scan.nextLine();
             System.out.println("--------------------------------");
+            //if the user inputs yes, calls addCollege() on list with resultsObj as the argument
             if (add.equals("yes")) {
                 list.addCollege(resultsObj);
             }
+            //prints a line asking the user if they want to go back to the search page
             System.out.println("1. Go Back to Previous Page\n2. Exit");
             System.out.print("Enter an integer: ");
             int choice = scan.nextInt();
             scan.nextLine();
+            //if the user inputs 2, bool is set to false, stopping the while loop and ending the method
             if (choice == 2) {
                 bool = false;
             }
         }
     }
 
+    //allows the user to search for colleges by taking in an input from the user in form of a String
     public static void search(CollegeList list) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("--------------------------------");
         System.out.print("Please enter a school to search for (no spaces): ");
         String name = scanner.nextLine();
+        //calls getSchoolInfo() with the user's input as an argument
         getSchoolInfo(name, list);
     }
 }
--- JavaAPIProject/src/main/java/com/example/CollegeList.java
+++ JavaAPIProject/src/main/java/com/example/CollegeList.java
@@ -8,46 +8,61 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class CollegeList {
+    //declares a private JSONObject ArrayList
     private ArrayList<JSONObject> list;
 
+    //constructor, intializes list to an empty ArrayList
     public CollegeList() {
         list = new ArrayList<JSONObject>();
     }
 
+    //add a JSONObject to list
     public void addCollege(JSONObject college) {
+        //iterates through list
         for (int i = 0; i < list.size(); i++) {
+            //if college is already in list, returns void
             if (college.getString("school.name").equals(list.get(i).getString("school.name"))) {
                 System.out.println("This school has already been added");
                 System.out.println("--------------------------------");
                 return;
             }
         }
+        //adds college to list
         list.add(college);
     }
 
+    //sets list to a new ArrayList, clearing list
     public void clear() {
         list = new ArrayList<JSONObject>();
     }
 
+    //removes a college at index idx
     public void removeCollege(int idx) {
         list.remove(idx);
     }
 
+    //returns list
     public ArrayList<JSONObject> getList() {
         return list;
     }
 
+    //returns the names of all colleges in list with an average of in state and out of state tuition to give the user an idea of how expensive their colleges are
     public void viewList() throws Exception {
         System.out.println("--------------------------------");
+        //calls sort() to sort the list by alphabetical order
         sort();
+        //iterates through list, printing the name of each school in list
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i).getString("school.name") + ", Index: " + i);
         }
+        //declares and initializes integers for calculating the average of tuition
         int inState = 0;
         int outOfState = 0;
         int inStateSize = 0;
         int outOfStateSize = 0;
+        //iterates through each element in list
         for (int j = 0; j < list.size(); j++) {
+            //uses try catch because sometimes the tuition is null so try catch prevents an exception
             try {
                 inState += list.get(j).getInt("latest.cost.tuition.in_state");
                 inStateSize++;
@@ -59,15 +74,19 @@ public class CollegeList {
             } catch (Exception e) {
             }
         }
+        //prints the average of in state and out of state tuition
         System.out.println("Average In State Tuititon: " + (double) inState/inStateSize);
         System.out.println("Average Out of State Tuition: " + (double) outOfState/outOfStateSize);
     }
 
+    //prints information of a specific college in list at index idx
     public void viewInfo(int idx) {
+        //prints information such as school name, city, state, admission rate, etc.
         System.out.println("--------------------------------");
         System.out.println("School Name: " + list.get(idx).getString("school.name"));
         System.out.println("City: " + list.get(idx).getString("school.city"));
         System.out.println("State: " + list.get(idx).getString("school.state"));
+        //uses try catch becomes some information is null, try catch prevents an exception
         try {
             System.out.println("Admission Rate: " + list.get(idx).getFloat("latest.admissions.admission_rate.overall"));
         } catch (Exception e) {
@@ -95,8 +114,10 @@ public class CollegeList {
         }
     }
 
+    //saves the information in list to a text file
     public void saveData() {
         try (FileWriter writer = new FileWriter("output.txt")) {
+            //iterates through list, saving each element in list to the text file output.txt in JSON text
             for (int i = 0; i < list.size(); i++) {
                 list.get(i).write(writer);
             }
@@ -105,17 +126,24 @@ public class CollegeList {
         }
     }
 
+    //loads the information from the text file output.txt
     public void loadData() {
         try (BufferedReader reader = new BufferedReader(new FileReader("output.txt"))) {
             String line;
             String text = "";
             while ((line = reader.readLine()) != null) {
+                //adds all the information to the String text
                 text += line;
             }
+            //splits text by the delimiter }, using positive lookbehind. learned from https://stackoverflow.com/questions/3481828/how-do-i-split-a-string-in-java
             String[] arr = text.split("(?<=})");
+            //checks if arr contains a String with }
             if (arr[0].indexOf("}") > -1) {
+                //iterates through each element in arr
                 for (int i = 0; i < arr.length; i++) {
+                    //declares and initializes a JSONObject with the String in arr at index i
                     JSONObject obj = new JSONObject(arr[i]);
+                    //adds obj to list
                     list.add(obj);
                 }
             }
@@ -124,10 +152,13 @@ public class CollegeList {
         }
     }
 
+    //sorts list by alphabetical order
     public void sort() {
+        //uses selection sort to sort
         for (int i = 0; i < list.size() - 1; i++) {
             int maxIndex = i;
             for (int j = i + 1; j < list.size(); j++) {
+                //uses compareTo to find with school is alphabetically higher
                 if (list.get(j).getString("school.name").compareTo(list.get(maxIndex).getString("school.name")) < 0) {
                     maxIndex = j;
                 }
